8da25c504a37dea53ed28faa27d077be4059096e
Patrick Wagstrom <160672+pridkett@users.noreply.github.com>
2025-02-24 16:41:57 -0500

    refactor: Simplify Tailwind config and add CSS file support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
   darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx,css}',
+  ],
   theme: {
-    extend: {
-      transitionProperty: {
-        'transform': 'transform',
-      },
-      rotate: {
-        'x-180': 'rotateX(180deg)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
+
+// export default {
+//   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], // Add all file types that use Tailwind classes
+//   darkMode: 'class',
+//   theme: {
+//     extend: {
+//       transitionProperty: {
+//         'transform': 'transform',
+//       },
+//       rotate: {
+//         'x-180': 'rotateX(180deg)',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
